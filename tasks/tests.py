9ee5a6d8213b0d95e9e561f4181da6a2cf9db142
zyx3998970 <zyx3998970@gmail.com>
2017-06-19 17:24:31 +0800

    WIP: add test for index view.

--- tasks/tests.py
+++ tasks/tests.py
@@ -1,3 +1,20 @@
+from django.core.urlresolvers import reverse
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Iteration
+
+
+# Test cases for models.
+
+# Test cases for views
+class IndexViewTests(TestCase):
+    def test_no_iteration(self):
+        response = self.client.get(reverse('tasks:index'))
+        self.assertEquals(response.status_code, 200)
+        self.assertContains(response, "No Tasks!")
+
+    def test_with_iterations(self):
+        Iteration.objects.create(name="IR102")
+        response = self.client.get(reverse('tasks:index'))
+        self.assertEquals(response.status_code, 200)
+        self.assertQuerysetEqual(response.context["iterations"], ["<Iteration: Iteration - IR102>"])
